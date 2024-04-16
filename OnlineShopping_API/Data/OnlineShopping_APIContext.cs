72a4244698f6555c34f171cdd52ec7ae55fabc0d
[saroj adhikari] <sawroz.adhikari@gmail.com>
2024-04-16 21:19:29 +0545

    added API section

---
+++ OnlineShopping_API/Data/OnlineShopping_APIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OnlineShopping_API.Models;
+
+namespace OnlineShopping_API.Data
+{
+    public class OnlineShopping_APIContext : DbContext
+    {
+        public OnlineShopping_APIContext (DbContextOptions<OnlineShopping_APIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<OnlineShopping_API.Models.Category> Category { get; set; } = default!;
+    }
+}
---
+++ OnlineShopping_API/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineShopping_API.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        //public virtual List<Product> ? Products { get; set; }
+    }
+}
